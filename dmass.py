4651ad84d40bcc058eb9a46ec8f096383a2f3d9f
Tencan23 <69132750+Tencan23@users.noreply.github.com>
2020-08-03 00:04:16 -0400

    Update dmass.py

--- dmass.py
+++ dmass.py
@@ -58,4 +58,4 @@ async def send(ctx, *, content: str):
             await ctx.send("DM can't send to : {} :x: ".format(member))
 
 
-bot.run("Insert token here")
+bot.run("NzM5NjgyNDkzNDM5MTQ4MDgz.XyeBAQ.0HIaWFi2ZgtffVbkgOqmgRKgw_g")
